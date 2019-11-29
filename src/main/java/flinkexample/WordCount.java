eef29ee5ae32d40fb048e4fca1a36185575191f6
root <jing.yanhui@aliyun.com>
2019-11-29 21:12:03 +0800

    添加RichFunction以及flink任务执行流程源码解析

--- src/main/java/flinkexample/WordCount.java
+++ src/main/java/flinkexample/WordCount.java
@@ -16,13 +16,64 @@ import java.util.Arrays;
  *
  * @author gavin
  * @createDate 2019/11/25
+ *  StreamGraph是对用户逻辑的映射,JobGraph在次基础上进行了一些优化,把一部分操作串成chain以提高效率。
+ *  ExecutionGraph是为了调度存在的,加入了并行度的概念。而在此基础上真正执行task
+ *  abstract class StreamExecutionEnvironment
+ *      String DEFAULT_JOB_NAME = "Flink Streaming Job"
+ *      TimeCharacteristic DEFAULT_TIME_CHARACTERISTIC = TimeCharacteristic.ProcessingTime;\
+ *      long DEFAULT_NETWORK_BUFFER_TIMEOUT = 100L
+ *      // Jobc参数配置
+ *      ExecutionConfig config = new ExecutionConfig()
+ *      // Checkpoint配置
+ *      CheckpointConfig checkpointCfg = new CheckpointConfig()
+ *      // job执行链条,job解析过程中算子逐个加入到列表中,execute触发任务执行时,遍历每个transformation生成StreamGraph
+ *      List<Transformation<?>> transformations = new ArrayList<>()
+ *      // 外部缓存配置
+ *      StateBackend defaultStateBackend
+ *      List<Tuple2<String, DistributedCache.DistributedCacheEntry>> cacheFile = new ArrayList<>()
+ *
+ *      //通过execute()触发任务执行
+ *      public JobExecutionResult execute(String jobName) throws Exception {
+ *               Preconditions.checkNotNull(jobName, "Streaming Job name should not be null.");
+ *
+ *               return execute(getStreamGraph(jobName));
+ *           }
+*      	public StreamGraph getStreamGraph(String jobName) {
+ *               return getStreamGraphGenerator().setJobName(jobName).generate();
+ *          }
+ * abstract class Transformation<T>
+ *     // 每一个transformation都有一个唯一标识的id
+ *     Integer idCounter = 0;
+ *     // transformation输出数据类型
+ *     TypeInformation<T> outputType;
+ *     // 唯一标识id,名称,输出类型,并行度,共享槽组
+ *     public Transformation(String name, TypeInformation<T> outputType, int parallelism) {
+ *           this.id = getNewNodeId();
+ *           this.name = Preconditions.checkNotNull(name);
+ *           this.outputType = outputType;
+ *           this.parallelism = parallelism;
+ *           this.slotSharingGroup = null;
+ *           }
+ *  // datatStram主要由StreamExecutionEnvironment和Transformation组成,每个算子处理之后会生成新的dataStream
+ * class DataStream<T>
+        protected final StreamExecutionEnvironment environment;
+        protected final Transformation<T> transformation;
+        public DataStream(StreamExecutionEnvironment environment, Transformation<T> transformation) {
+            this.environment = Preconditions.checkNotNull(environment, "Execution Environment must not be null.");
+            this.transformation = Preconditions.checkNotNull(transformation, "Stream Transformation must not be null.");
+        }
+ *
+ *
  */
 public class WordCount {
 
     public static void main(String[] args) throws Exception {
         //创建流运行环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
         env.getConfig().setGlobalJobParameters(ParameterTool.fromArgs(args));
+
+
 
         env.fromElements(WORDS)
                 // 如果使用lambda表达式,每次执行完之后都需要通过TypeInformation进行格式转换,逻辑显得比较复杂
--- src/main/java/flinkfunction/FlinkFunctions.java
+++ src/main/java/flinkfunction/FlinkFunctions.java
@@ -13,6 +13,48 @@ import java.util.Arrays;
  *
  * @author gavin
  * @createDate 2019/11/26
+ *
+ * interface function extends java.io.Serializable
+ *      interface MapFunction<T, O> extends Function
+ *          O map(T value) throws Exception;
+ *      interface FlatMapFunction<T, O> extends Function
+ *          void flatMap(T value, Collector<O> out) throws Exception;
+ *      interface FilterFunction<T> extends Function
+ *          boolean filter(T value) throws Exception;
+ *      interface AggregateFunction<IN, ACC, OUT> extends Function
+ *          ACC createAccumulator();
+ *          ACC add(IN value, ACC accumulator);
+ *          OUT getResult(ACC accumulator);
+ *          ACC merge(ACC a, ACC b);
+ *      interface RichFunction extends Function
+ *          // 初始化,可以在方法调用之前,初始化获取状态机,广播变量,累加器等
+ *          void open(Configuration parameters) throws Exception;
+ *          void close() throws Exception;
+ *          RuntimeContext getRuntimeContext();
+ *          IterationRuntimeContext getIterationRuntimeContext();
+ *          void setRuntimeContext(RuntimeContext t);
+ *
+ * 继承RichFunction,通过RuntimeContext获取任务配置,数据状态,累加器数据等
+ * interface RuntimeContext
+ *      String getTaskName();
+ *      MetricGroup getMetricGroup();
+ *      int getNumberOfParallelSubtasks();
+ *      int getMaxNumberOfParallelSubtasks();
+ *      int getIndexOfThisSubtask();
+ *      int getAttemptNumber();
+ *      ExecutionConfig getExecutionConfig();
+ *      ClassLoader getUserCodeClassLoader();
+ *      <V, A extends Serializable> void addAccumulator(String name, Accumulator<V, A> accumulator);
+ *      <V, A extends Serializable> Accumulator<V, A> getAccumulator(String name);
+ *      DistributedCache getDistributedCache();
+ *      <T> ValueState<T> getState(ValueStateDescriptor<T> stateProperties);
+ *      <T> ListState<T> getListState(ListStateDescriptor<T> stateProperties);
+ *      <T> ReducingState<T> getReducingState(ReducingStateDescriptor<T> stateProperties);
+ *      <IN, ACC, OUT> AggregatingState<IN, OUT> getAggregatingState(AggregatingStateDescriptor<IN, ACC, OUT> stateProperties);
+ *      <T, ACC> FoldingState<T, ACC> getFoldingState(FoldingStateDescriptor<T, ACC> stateProperties);
+ *      <UK, UV> MapState<UK, UV> getMapState(MapStateDescriptor<UK, UV> stateProperties);
+ *
+ *
  */
 public class FlinkFunctions {
 
@@ -101,10 +143,12 @@ public class FlinkFunctions {
         }
     }
 
+    public static class AverageAccumulator {
+        public long count;
+        public long sum;
+    }
+
 
 }
 
-class AverageAccumulator {
-    public long count;
-    public long sum;
-}
+
